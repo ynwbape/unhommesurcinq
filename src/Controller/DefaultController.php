57fbb99271ab9fe9bfd225da0faeba302de5b65b
UnHommeSurCinq <eric.air@un-homme-sur-cinq.fr>
2021-12-07 04:54:09 +0100

    Main pages

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -17,6 +17,46 @@ class DefaultController extends AbstractController
     }
 
     /**
+     * @Route("/ressources-gratuites", name="ressources_gratuites")
+     */
+    public function freeRessources(): Response
+    {
+        return $this->render('free_ressources.html.twig');
+    }
+
+    /**
+     * @Route("/appel-gratuit", name="appel_gratuit")
+     */
+    public function phoneCallBooking(): Response
+    {
+        return $this->render('phone_call_booking.html.twig');
+    }
+
+    /**
+     * @Route("/presentation", name="about")
+     */
+    public function about(): Response
+    {
+        return $this->render('about.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('contact.html.twig');
+    }
+
+    /**
+     * @Route("/produits", name="products")
+     */
+    public function products(): Response
+    {
+        return $this->render('products.html.twig');
+    }
+
+    /**
      * @Route("/landkit", name="landkit")
      */
     public function landkit(): Response
